56e578ecbaf72ac2bc5de7d0a6b99ff3dcb54264
Jonas Waage <jonas.h.waage@gmail.com>
2020-09-05 00:57:08 +0200

    Add row and column labels, + tests, DSL version

--- game1000-solver/src/main/kotlin/gametheory/TwoPlayerGame.kt
+++ game1000-solver/src/main/kotlin/gametheory/TwoPlayerGame.kt
@@ -2,26 +2,34 @@ package gametheory
 
 import bimatrix.R
 import lcp.Rational
+import java.lang.RuntimeException
 
 /*
  * Copyright (c) Jonas Waage 04/09/2020
  */
 data class Payoff(val a : Rational,val b : Rational)
 
-class GameScope(val col : ArrayList<ArrayList<Payoff>>) {
+class GameScope(private val rows : ArrayList<ArrayList<Payoff>>,
+                private val rowMapping : MutableMap<String,Int>,
+                private val colMapping : MutableMap<String,Int>) {
 
-    fun id(vararg label : String) {
-
+    fun columnLabels(vararg label : String) {
+        label.forEachIndexed { idx,lbl->
+            colMapping[lbl] = idx
+        }
     }
 
     fun row(init : RowScope.() -> Unit) {
         val row = ArrayList<Payoff>()
         init(RowScope(row))
-        col.add(row);
+        rows.add(row);
     }
 
     fun row(id:String,init : RowScope.() -> Unit) {
-
+        rowMapping[id] = rows.size
+        val row = ArrayList<Payoff>()
+        init(RowScope(row))
+        rows.add(row);
     }
 }
 
@@ -31,42 +39,80 @@ class RowScope(val row : ArrayList<Payoff>) {
     fun p(a:Int,b:Int) {
         row.add(Payoff(a.R,b.R))
     }
+    
 }
 
 enum class Player {ROW,COLUMN}
 
-class TwoPlayerGame(init : GameScope.() -> Unit) {
-    val rows = ArrayList<ArrayList<Payoff>>()
-    val cols : ArrayList<ArrayList<Payoff>>
+class GameException(msg:String) : RuntimeException(msg)
+
+class TwoPlayerGame private constructor(
+        private val rows : List<List<Payoff>>,
+        private val rowMap : Map<String,Int>,
+        private val colMap : Map<String,Int>) {
+    private val cols : ArrayList<ArrayList<Payoff>>
+
+    companion object {
+        fun create(vararg init : RowScope.() -> Unit) : TwoPlayerGame {
+            val rows = ArrayList<ArrayList<Payoff>>()
+            init.map { i -> val r = ArrayList<Payoff>(); rows.add(r); i( RowScope(r) ) }
+            return TwoPlayerGame(rows, emptyMap(), emptyMap())
+        }
+
+        fun create(init :GameScope.() -> Unit) : TwoPlayerGame {
+            val rows = ArrayList<ArrayList<Payoff>>()
+            val rowMap = mutableMapOf<String,Int>()
+            val colMap = mutableMapOf<String,Int>()
+            init(GameScope(rows,rowMap,colMap))
+            return TwoPlayerGame(rows,rowMap,colMap)
+        }
+    }
 
     init {
-        init(GameScope(rows))
-        assert(rows.size > 0) {"Must have more then 0 rows"}
-        assert(rows[0].size > 0) {"Must have more then 0 columns"}
-        assert(rows.map { i -> i.size }.distinct().size==1) { "All rows must have the same mount of values" }
+        assert(rows.isNotEmpty()) {"Must have more then 0 rows"}
+        assert(rows[0].isNotEmpty()) {"Must have more then 0 columns"}
+        assert(rows.map { i -> i.size }.distinct().size == 1) { "All rows must have the same mount of values" }
 
         //Precalculate columns
         val columns = ArrayList<ArrayList<Payoff>>()
-        (0 until rows[0].size).forEach { i ->
+        (rows[0].indices).forEach { i ->
             val column = ArrayList<Payoff>()
-            (0 until rows.size).forEach { j ->
+            (rows.indices).forEach { j ->
                 column.add(rows[j][i])
             }
             columns.add(column)
         }
+
         cols = columns
+
+        if(rowMap.isNotEmpty()) {
+            assert(rowMap.keys.size == rows.size)
+            {"Row labels has wrong size ${rowMap.keys.size} should be the same as number of rows ${rows.size} "}
+        }
+
+        if(colMap.isNotEmpty()) {
+            assert(colMap.keys.size == cols.size)
+            {"Column labels has wrong size ${colMap.keys.size} should be the same as number of rows ${cols.size} "}
+
+        }
     }
 
     override fun toString(): String {
         return "TwoPlayerGame(rows=$rows)"
     }
 
-    fun hasStronglyDominantStrategy(player : Player) {
 
-    }
-
-    fun hasWeaklyDominantStrategy(player : Player) {
-
+    fun nashEquilibriums() : List<Pair<Int,Int>> {
+        return rows.indices.flatMap {i->
+            cols.indices.flatMap { j->
+                if(isNashEquilibrium(i,j)) {
+                    listOf(Pair(i,j))
+                }
+                else {
+                    emptyList()
+                }
+            }
+        }
     }
 
     private fun conditionTrueForAll(player : Player, id: Int, condition : (Rational, Rational) -> Boolean ) : Boolean {
@@ -92,15 +138,34 @@ class TwoPlayerGame(init : GameScope.() -> Unit) {
                 getRow(id).mapIndexed { idx, p ->
                     getColumn(idx).filterIndexed { rIdx, _ -> rIdx != id }
                             .map { i -> condition(p.a,i.a) }
-                }.flatten().any()
+                }.flatten().any {it}
             }
             Player.COLUMN -> {
                 getColumn(id).mapIndexed { idx, p ->
                     getRow(idx).filterIndexed { rIdx, _ -> rIdx != id }
                             .map { i -> condition(p.b,i.b) }
-                }.flatten().any()
+                }.flatten().any {it}
             }
         }
+    }
+
+    fun getRowOrColumn(player:Player, id :String) : Int {
+        return when(player) {
+            Player.ROW -> getRow(id)
+            Player.COLUMN -> getColumn(id)
+        }
+    }
+
+    fun getRow(id:String) : Int {
+        return rowMap[id]?:throw GameException("Row label $id is not defined")
+    }
+
+    fun getColumn(id:String) : Int {
+        return colMap[id]?:throw GameException("Columns label $id is not defined")
+    }
+
+    fun isNashEquilibrium(row : String,col : String) : Boolean {
+        return isNashEquilibrium(getRow(row),getColumn(col))
     }
 
     fun isNashEquilibrium(row : Int,col : Int) : Boolean {
@@ -111,12 +176,26 @@ class TwoPlayerGame(init : GameScope.() -> Unit) {
         return colCondition && rowCondition
     }
 
+    fun isStronglyDominantStrategy(player : Player, id : String) : Boolean {
+        return isStronglyDominantStrategy(player,getRowOrColumn(player,id))
+    }
+
     fun isStronglyDominantStrategy(player : Player, id : Int) : Boolean {
         return conditionTrueForAll(player,id) { a, b -> a > b }
     }
 
+    fun isWeaklyDominantStrategy(player : Player, id : String) : Boolean {
+        return isWeaklyDominantStrategy(player,getRowOrColumn(player,id))
+    }
+
     fun isWeaklyDominantStrategy(player : Player, id : Int) : Boolean {
-        return conditionTrueForAll(player,id) { a,b -> a >= b } && conditionTrueForOne(player,id) { a,b -> a > b }
+        val all = conditionTrueForAll(player,id) { a,b -> a >= b }
+        val one = conditionTrueForOne(player,id) { a,b -> a > b }
+        return all && one
+    }
+
+    fun isVeryWeaklyDominantStrategy(player : Player, id : String) : Boolean {
+        return isVeryWeaklyDominantStrategy(player,getRowOrColumn(player,id))
     }
 
     fun isVeryWeaklyDominantStrategy(player : Player, id : Int) : Boolean {
--- game1000-solver/src/test/kotlin/gametheory/TwoPlayerGameTest.kt
+++ game1000-solver/src/test/kotlin/gametheory/TwoPlayerGameTest.kt
@@ -12,94 +12,94 @@ class TwoPlayerGameTest {
 
     @Test
     fun testGetRowCol() {
-        val vl = TwoPlayerGame {
-            row { p(1, 2) ; p(0, 4) }
-            row { p(1,2) ; p(0,2) }
-        }
+        val game = TwoPlayerGame.create (
+                { p(1, 2); p(0, 4) },
+                { p(1, 2); p(0, 2) }
+        )
 
-        assertEquals(1.R,vl.getRow(0)[0].a)
-        assertEquals(2.R,vl.getRow(0)[0].b)
+        assertEquals(1.R,game.getRow(0)[0].a)
+        assertEquals(2.R,game.getRow(0)[0].b)
 
-        assertEquals(0.R,vl.getColumn(1)[0].a)
-        assertEquals(4.R,vl.getColumn(1)[0].b)
+        assertEquals(0.R,game.getColumn(1)[0].a)
+        assertEquals(4.R,game.getColumn(1)[0].b)
 
-        assertEquals(0.R,vl.getColumn(1)[1].a)
-        assertEquals(2.R,vl.getColumn(1)[1].b)
+        assertEquals(0.R,game.getColumn(1)[1].a)
+        assertEquals(2.R,game.getColumn(1)[1].b)
 
-        assertEquals(2.R,vl.getColumn(0)[1].b)
-        assertEquals(4.R,vl.getColumn(1)[0].b)
+        assertEquals(2.R,game.getColumn(0)[1].b)
+        assertEquals(4.R,game.getColumn(1)[0].b)
     }
 
 
     @Test
     fun stronglyDominantTest() {
-        val vl = TwoPlayerGame {
-            row { p(4, 2) ; p(3, 1) }
-            row { p(2,2) ; p(2,2) }
-        }
+        val game = TwoPlayerGame.create (
+             { p(4, 2) ; p(3, 1) },
+             { p(2,2) ; p(2,2) }
+        )
 
-        assertEquals(true,vl.isStronglyDominantStrategy(Player.ROW,0))
-        assertEquals(false,vl.isStronglyDominantStrategy(Player.ROW,1))
-        assertEquals(false,vl.isStronglyDominantStrategy(Player.COLUMN,0))
-        assertEquals(false,vl.isStronglyDominantStrategy(Player.COLUMN,1))
+        assertEquals(true,game.isStronglyDominantStrategy(Player.ROW,0))
+        assertEquals(false,game.isStronglyDominantStrategy(Player.ROW,1))
+        assertEquals(false,game.isStronglyDominantStrategy(Player.COLUMN,0))
+        assertEquals(false,game.isStronglyDominantStrategy(Player.COLUMN,1))
 
-        val vl2 = TwoPlayerGame {
-            row { p(4, 4) ; p(3, 1) }
-            row { p(2,3) ; p(2,2) }
-        }
+        val game2 = TwoPlayerGame.create (
+             { p(4, 4) ; p(3, 1) },
+             { p(2,3) ; p(2,2) }
+        )
 
-        assertEquals(true,vl2.isStronglyDominantStrategy(Player.ROW,0))
-        assertEquals(false,vl2.isStronglyDominantStrategy(Player.ROW,1))
-        assertEquals(true,vl2.isStronglyDominantStrategy(Player.COLUMN,0))
-        assertEquals(false,vl2.isStronglyDominantStrategy(Player.COLUMN,1))
+        assertEquals(true,game2.isStronglyDominantStrategy(Player.ROW,0))
+        assertEquals(false,game2.isStronglyDominantStrategy(Player.ROW,1))
+        assertEquals(true,game2.isStronglyDominantStrategy(Player.COLUMN,0))
+        assertEquals(false,game2.isStronglyDominantStrategy(Player.COLUMN,1))
     }
 
 
     @Test
     fun weaklyDominantTest() {
-        val vl = TwoPlayerGame {
-            row { p(4, 2) ; p(3, 1) }
-            row { p(2,2) ; p(2,2) }
-        }
+        val game = TwoPlayerGame.create (
+                { p(4, 2) ; p(3, 1) },
+                { p(2,2) ; p(2,2) }
+        )
 
-        assertEquals(true,vl.isWeaklyDominantStrategy(Player.ROW,0))
-        assertEquals(false,vl.isWeaklyDominantStrategy(Player.ROW,1))
-        assertEquals(true,vl.isWeaklyDominantStrategy(Player.COLUMN,0))
-        assertEquals(false,vl.isWeaklyDominantStrategy(Player.COLUMN,1))
+        assertEquals(true,game.isWeaklyDominantStrategy(Player.ROW,0))
+        assertEquals(false,game.isWeaklyDominantStrategy(Player.ROW,1))
+        assertEquals(true,game.isWeaklyDominantStrategy(Player.COLUMN,0))
+        assertEquals(false,game.isWeaklyDominantStrategy(Player.COLUMN,1))
 
-        val vl2 = TwoPlayerGame {
-            row { p(4, 4) ; p(3, 1) }
-            row { p(2,3) ; p(2,2) }
-        }
+        val game2 = TwoPlayerGame.create (
+                { p(4, 4) ; p(3, 1) },
+                { p(2,3) ; p(2,2) }
+        )
 
-        assertEquals(true,vl2.isWeaklyDominantStrategy(Player.ROW,0))
-        assertEquals(false,vl2.isWeaklyDominantStrategy(Player.ROW,1))
-        assertEquals(true,vl2.isWeaklyDominantStrategy(Player.COLUMN,0))
-        assertEquals(false,vl2.isWeaklyDominantStrategy(Player.COLUMN,1))
+        assertEquals(true,game2.isWeaklyDominantStrategy(Player.ROW,0))
+        assertEquals(false,game2.isWeaklyDominantStrategy(Player.ROW,1))
+        assertEquals(true,game2.isWeaklyDominantStrategy(Player.COLUMN,0))
+        assertEquals(false,game2.isWeaklyDominantStrategy(Player.COLUMN,1))
     }
 
 
     @Test
     fun veryWeaklyDominantTest() {
-        val vl = TwoPlayerGame {
-            row { p(4, 2) ; p(3, 2) }
-            row { p(2,2) ; p(2,2) }
+        val game = TwoPlayerGame.create {
+            row { p(4, 2); p(3, 2) }
+            row { p(2, 2); p(2, 2) }
         }
 
-        assertEquals(true,vl.isVeryWeaklyDominantStrategy(Player.ROW,0))
-        assertEquals(false,vl.isVeryWeaklyDominantStrategy(Player.ROW,1))
-        assertEquals(true,vl.isVeryWeaklyDominantStrategy(Player.COLUMN,0))
-        assertEquals(true,vl.isVeryWeaklyDominantStrategy(Player.COLUMN,1))
+        assertEquals(true,game.isVeryWeaklyDominantStrategy(Player.ROW,0))
+        assertEquals(false,game.isVeryWeaklyDominantStrategy(Player.ROW,1))
+        assertEquals(true,game.isVeryWeaklyDominantStrategy(Player.COLUMN,0))
+        assertEquals(true,game.isVeryWeaklyDominantStrategy(Player.COLUMN,1))
 
-        val vl2 = TwoPlayerGame {
-            row { p(4, 4) ; p(3, 1) }
-            row { p(2,3) ; p(2,2) }
+        val game2 = TwoPlayerGame.create {
+            row { p(4, 4); p(3, 1) }
+            row { p(2, 3); p(2, 2) }
         }
 
-        assertEquals(true,vl2.isVeryWeaklyDominantStrategy(Player.ROW,0))
-        assertEquals(false,vl2.isVeryWeaklyDominantStrategy(Player.ROW,1))
-        assertEquals(true,vl2.isVeryWeaklyDominantStrategy(Player.COLUMN,0))
-        assertEquals(false,vl2.isVeryWeaklyDominantStrategy(Player.COLUMN,1))
+        assertEquals(true,game2.isVeryWeaklyDominantStrategy(Player.ROW,0))
+        assertEquals(false,game2.isVeryWeaklyDominantStrategy(Player.ROW,1))
+        assertEquals(true,game2.isVeryWeaklyDominantStrategy(Player.COLUMN,0))
+        assertEquals(false,game2.isVeryWeaklyDominantStrategy(Player.COLUMN,1))
     }
 
 
@@ -109,26 +109,88 @@ class TwoPlayerGameTest {
     fun nashEquilibriumTest() {
 
         //Prisoners dilemma
-        val vl = TwoPlayerGame {
-            row { p(-2, -2) ; p(-8, -1) }
-            row { p(-1,-8) ; p(-5,-5) }
-        }
+        val game = TwoPlayerGame.create (
+                { p(-2, -2); p(-8, -1) },
+                { p(-1, -8); p(-5, -5) }
+        )
 
-        assertEquals(false,vl.isNashEquilibrium(0,0))
-        assertEquals(false,vl.isNashEquilibrium(0,1))
-        assertEquals(false,vl.isNashEquilibrium(1,0))
-        assertEquals(true,vl.isNashEquilibrium(1,1))
+        assertEquals(false,game.isNashEquilibrium(0,0))
+        assertEquals(false,game.isNashEquilibrium(0,1))
+        assertEquals(false,game.isNashEquilibrium(1,0))
+        assertEquals(true,game.isNashEquilibrium(1,1))
 
         //BOS
-        val vl2 = TwoPlayerGame {
-            row { p(2, 1) ; p(0, 0) }
-            row { p(0,0) ; p(1,2) }
+        val game2 = TwoPlayerGame.create (
+                { p(2, 1); p(0, 0) },
+                { p(0, 0); p(1, 2) }
+        )
+
+        assertEquals(true,game2.isNashEquilibrium(0,0))
+        assertEquals(false,game2.isNashEquilibrium(0,1))
+        assertEquals(false,game2.isNashEquilibrium(1,0))
+        assertEquals(true,game2.isNashEquilibrium(1,1))
+    }
+
+
+    @Test
+    fun `single not weakly dominant nash equilibrium`() {
+
+        val game = TwoPlayerGame.create {
+            columnLabels("A","B","C")
+            row("A") { p(-1, -1); p(0, 0); p(1, 1) }
+            row("B") { p(0, 0); p(-1, -1); p(-1, -1) }
+            row("C") { p(1, 1); p(-1, -1); p(2, 2) }
         }
 
-        assertEquals(true,vl2.isNashEquilibrium(0,0))
-        assertEquals(false,vl2.isNashEquilibrium(0,1))
-        assertEquals(false,vl2.isNashEquilibrium(1,0))
-        assertEquals(true,vl2.isNashEquilibrium(1,1))
+        assertEquals(1,game.nashEquilibriums().size)
+        assertEquals(false,game.isNashEquilibrium("A","C"))
+        assertEquals(false,game.isNashEquilibrium("C","A"))
+
+        assertEquals(true,game.isNashEquilibrium("C","C"))
+        assertEquals(false,game.isVeryWeaklyDominantStrategy(Player.ROW,"C"))
+        assertEquals(false,game.isVeryWeaklyDominantStrategy(Player.COLUMN,"C"))
+
+    }
+
+    @Test
+    fun `a single weakly dominant equilibrium`() {
+
+        val game = TwoPlayerGame.create {
+            columnLabels("A","B","C")
+            row("A") { p(-1, -1); p(0, 0); p(1, 1) }
+            row("B") { p(0, 0); p(-1, -1); p(0, 0) }
+            row("C") { p(1, 1); p(0, 0); p(2, 2) }
+        }
+
+        val eq = game.nashEquilibriums()
+
+        assertEquals(1,eq.size)
+        assertEquals(true,game.isNashEquilibrium("C","C"))
+        assertEquals(true,game.isWeaklyDominantStrategy(Player.ROW,"C"))
+        assertEquals(false,game.isStronglyDominantStrategy(Player.ROW,"C"))
+
+    }
+
+
+
+    @Test
+    fun `single weak or equilibirum, and one only nash`() {
+
+        val game = TwoPlayerGame.create {
+            columnLabels("A","B")
+            row("A") { p(2, 2); p(2, 2) }
+            row("B") { p(2, 2); p(3, 3) }
+        }
+
+        val eq = game.nashEquilibriums()
+
+        assertEquals(2,eq.size)
+        assertEquals(true,game.isNashEquilibrium("A","A"))
+        assertEquals(true,game.isNashEquilibrium("B","B"))
+
+        assertEquals(true,game.isWeaklyDominantStrategy(Player.ROW,"B"))
+        assertEquals(false,game.isWeaklyDominantStrategy(Player.ROW,"A"))
+
     }
 
 
